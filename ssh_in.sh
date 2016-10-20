59a7413b46459703110208dadc78b92e667af80c
Katrina Wijaya <katrina@thewijayas.com>
2016-10-20 14:53:49 -0700

    Removed personal username

--- ssh_in.sh
+++ ssh_in.sh
@@ -4,12 +4,12 @@ server=$(echo $1 | sed 's/^0*//')
 
 if [ $# -eq 1 ]; then
   if [ $1 -gt 0 ] && [ $1 -lt 10 ]; then
-    ssh wijaya@lnxsrv0$server.seas.ucla.edu
+    ssh user@lnxsrv0$server.seas.ucla.edu
   else
     echo "Input a number from 1-9. You inputted $1."
   fi
 elif [ $# -eq 0 ]; then
-  ssh wijaya@lnxsrv07.seas.ucla.edu
+  ssh user@lnxsrv07.seas.ucla.edu
 else
   echo "Too many arguments! Only one input is allowed, a number from 1-9."
 fi
